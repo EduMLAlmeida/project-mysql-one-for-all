adca6041c64caf62a41ddb5a13af568720d450bf
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-06-07 11:16:08 -0300

    Atende requisito 7.

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT art.artist_name AS artista,
+alb.album_name AS album,
+COUNT(fol.user_id) AS seguidores FROM SpotifyClone.artists AS art
+INNER JOIN SpotifyClone.albuns AS alb
+ON art.artist_id=alb.artist_id
+INNER JOIN SpotifyClone.follows AS fol
+ON art.artist_id=fol.artist_id
+GROUP BY album, artista
+ORDER BY seguidores DESC;
